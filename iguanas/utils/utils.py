7240e88b1b96e4f60a962599f793259bd8e8ebad
James Laidler <jlaidler@paypal.com>
2022-01-17 14:45:53 +0000

    Updated setup.py for python>=3.7

--- iguanas/utils/utils.py
+++ iguanas/utils/utils.py
@@ -561,6 +561,8 @@ def check_allowed_types(x: object, x_name: str,
     """
 
     x_type = str(type(x))
+    if not x_type.startswith('<class'):
+        x_type = f"<class '{x_type}'>"
     if x_type not in allowed_types:
         allowed_types_str = ' or '.join(
             [allowed_type.split("'")[1] for allowed_type in allowed_types])
@@ -588,4 +590,6 @@ def is_type(x: object, types: List[str]) -> bool:
     """
 
     x_type = str(type(x))
+    if not x_type.startswith('<class'):
+        x_type = f"<class '{x_type}'>"
     return x_type in types
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
