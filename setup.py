2e38ca8cc1564c6f25b6730d98d49cf543a70f0a
James Laidler <jlaidler@paypal.com>
2022-02-10 18:20:31 +0000

    Updated setup.py for seaborn>=0.9.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
         # 'scikit-learn==0.23.2', 'scipy==1.*'
         'category-encoders>=2.0.0',
         # 'matplotlib==3.*',
-        # 'seaborn==0.9.0',
+        'seaborn>=0.9.0',
         # 'numpy>=1.21',
         'pandas>=1.1.4',
         'hyperopt>=0.2.5',
