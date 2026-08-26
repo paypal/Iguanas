594962cc317b1cfd99664070a17a988d36261319
Charles Poli <cpoli@paypal.com>
2026-08-26 14:27:04 +0100

    mod pyproject.toml to avoid infinite loop with timezone.utc

--- iguanas/rule_registry.py
+++ iguanas/rule_registry.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import builtins
 import json
-from datetime import timezone, datetime
+from datetime import datetime, timezone
 from pathlib import Path
 from typing import Any
 
